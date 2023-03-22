da90103a20612ba02c4faa8e9368fa1969d0002e
Martin Hughes <martin-hughes@users.noreply.github.com>
2023-03-22 18:33:17 +0000

    Update failing test

--- test/tests/ech.cpp
+++ test/tests/ech.cpp
@@ -13,23 +13,18 @@ TEST(EchTests, ClearFromEnd) {
   EXPECT_EQ("", vt.get_line_text(1));
 }
 
-#if 0
 TEST(EchTests, ClearMultipleFromMidLine) {
-  TMT *vt = tmt_open(80, 25, normal_callback, nullptr, nullptr);
-  TMT &vt_r = *vt;
+  TestVtWrapper vt(80, 25);
 
-  write_string(vt_r, "Hello, world");
-  write_cursor_move(vt_r, CursorMoveDir::Left, 5);
+  write_string(vt, "Hello, world");
+  write_cursor_move(vt, CursorMoveDir::Left, 5);
 
   // Cursor is now underneath 'w' of 'world'
-  write_csi(vt_r);
-  write_string(vt_r, "3X");
+  write_csi(vt);
+  write_string(vt, "3X");
 
   // should have cleared the letters 'wor'
 
-  EXPECT_TRUE(line_is_equal_to(*vt, 0, "Hello,    ld"));
-  EXPECT_TRUE(line_is_equal_to(*vt, 1, ""));
-
-  tmt_close(vt);
+  EXPECT_EQ("Hello,    ld", vt.get_line_text(0));
+  EXPECT_EQ("", vt.get_line_text(1));
 }
-#endif
